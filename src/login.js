e7720458108d967b1240eda9b08e39117b8e373a
Castro Daniel <104678727+cedamo9000@users.noreply.github.com>
2022-11-16 07:01:08 -0800

    validation

---
+++ src/login.js
@@ -0,0 +1,11 @@
+function validate(){
+  var email=document.getElementById('email');
+  var password=document.getElementById('password');
+
+  if(email==""){
+    document.querySelector('error').style.display="none";
+  }
+  else{
+    document.querySr('error').style.display="none";
+  }
+}
